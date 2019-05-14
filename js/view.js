d16f1e0db53bb7540e08aea67d7d562ac7003413
willy <shen.willy95@gmail.com>
2019-05-14 23:33:35 +0700

    updated form field validation

--- js/view.js
+++ js/view.js
@@ -1,6 +1,51 @@
 $(function(){
+	validator = $('#form-identity').validate({
+	    rules: {
+	      input_name: {
+	        required: true
+	      },
+	      input_email: {
+	        required: true
+	      },
+	      input_ethereum: {
+	        required: true
+	      },
+	      input_twitter: {
+	        required: true
+	      },
+	      input_discord: {
+	        required: true
+	      },
+	    },
+	    messages: {
+	      input_name: {
+	        required: 'Name must not be empty',
+	      },
+	      input_email: {
+	        required: 'Email address must not be empty',
+	      },
+	      input_ethereum: {
+	        required: 'Ethereum address must not be empty',
+	      },
+	      input_twitter: {
+	        required: 'Twitter handle must not be empty',
+	      },
+	      input_discord: {
+	        required: 'Discord ID must not be empty',
+	      },
+
+	    },
+		onfocusout: false,
+		onkeyup: false,
+		onclick: false
+	  });
+	
+
 	$(".btn-save").click(function() {
 		let input_field = $(this).parent().find("input");
+
+		if (!input_field.valid()) return;
+
 		let type = input_field.attr("data");
 		let value = input_field.val();
 		save_identity_field(type, value);
